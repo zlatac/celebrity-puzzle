a69b6a4a74a90c98e86433fa1b9e4fc3264586e6
zlatac <[idahosa1adaghe@hotmail.ca]>
2025-10-17 01:21:19 -0400

    SV-41. Fix ts error & add settings to class

--- globals.d.ts
+++ globals.d.ts
@@ -5,16 +5,6 @@ declare global {
   interface Window {
     idaStockVision: IStockVision;
     idaStockVisionTrade: IStockVisionTrade;
-    projectStockVision: Function<MutationCallback>;
-    ProjectStockVision: {
-      vision: Function<MutationCallback>;
-      visionLarge: Function<String>;
-      visionSmall: Function<String>;
-      visionTiny: Function<String>;
-      visionManual: Function<String>;
-      visionTools: IStockVision.tools;
-      trade: Function;
-    };
     stockVisionTrade: Function;
   }
 }
